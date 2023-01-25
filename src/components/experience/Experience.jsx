bb18d3635724f2fae3d79b71cccc613ea4ad7833
Roxana Bita <roxana.bita@gmail.com>
2023-01-25 23:33:37 +0100

    finish skill section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
 
 const Experience = () => {
-  return (
-    <section id="experience">
-      
-    </section>
-  )
-}
+    const blocks = {
+        Frontend: ["HTML", "CSS", "JavaScript", "React", "Tailwind", "Bootstrap"],
+        Backend: ["Node JS", "Express JS", "MongoDB", "PostgreSQL"],
+        Methodologies: ["Git", "Scrum", "Kanban", "UI/UX Design"],
+        Tools: ["Adobe CC", "Wordpress", "Typo3", "Figma", "Github"],
+    };
 
-export default Experience
+    const getSkills = (obj) => Object.keys(obj);
+    console.log(getSkills(blocks));
+    return (
+        <section id='experience'>
+            <p>The Skills I Have</p>
+            <h2>My Experience</h2>
+
+            <div className='container experience__container'>
+                {getSkills(blocks).map((skill) => (
+                    <div className='experience__skillset'>
+                        <h3>{skill}</h3>
+                        <ul className='experience__list'>
+                            {blocks[skill].map((s) => (
+                                <li key={skill} className='experience__li'>
+                                    <i class='fa fa-check-circle' aria-hidden='true'></i>
+                                    <span>{s}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+
+                {/* <div className='experience__frontend'></div>
+
+                <div className='experience__backend'></div>
+
+                <div className='experience__methodologies'></div>
+
+                <div className='experience__tools'></div> */}
+            </div>
+        </section>
+    );
+};
+
+export default Experience;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,29 +12,11 @@ const navlinks=[{'name':'', 'icon':'home'}, {'name':'about', 'icon':'user'}, {'n
         <nav>
           {navlinks.map((section)=> (
             <a href={'#'+section.name} onClick={()=>setActiveNav('#'+ section.name)} className={activeNav==='#'+section.name? 'active' : ''}>
-                <i class={`active fa fa-${section.icon} fa-2x`} aria-hidden='true'></i>
+                <i class={`active fa fa-${section.icon}`} aria-hidden='true'></i>
             </a>)
           )}
            
 
-
-
-
-            {/* <a href='#about' onClick={()=>setActiveNav('#about')} className={activeNav==='#about'? 'active' : ''}>
-                <i class='fa fa-user fa-2x' aria-hidden='true'></i>
-            </a>
-            <a href='#experience' onClick={()=>setActiveNav('#')} className={activeNav==='#'? 'active' : ''}>
-                <i class='fa fa-briefcase fa-2x' aria-hidden='true'></i>
-            </a>
-            <a href='#portfolio' onClick={()=>setActiveNav('#')} className={activeNav==='#'? 'active' : ''}>
-                <i class='fa fa-picture-o fa-2x' aria-hidden='true'></i>
-            </a>
-            <a href='#services' onClick={()=>setActiveNav('#')} className={activeNav==='#'? 'active' : ''}>
-                <i class='fa fa-handshake-o fa-2x' aria-hidden='true'></i>
-            </a>
-            <a href='#contact' onClick={()=>setActiveNav('#')} className={activeNav==='#'? 'active' : ''}>
-                <i class='fa fa-paper-plane fa-2x' aria-hidden='true'></i> */}
-            {/* </a> */}
         </nav>
     );
 };
