3328927cb259ab111fdf6383d76c9b56010ea161
Roxana Bita <roxana.bita@gmail.com>
2023-02-17 15:01:02 +0100

    add mockup of news app

--- src/components/porfolio/Portfolio.jsx
+++ src/components/porfolio/Portfolio.jsx
@@ -9,37 +9,37 @@ const Portfolio = () => {
             image: BB,
             title: "Bookbandits",
             description:
-                "A book-sharing platform made easy: use your webcam to scan your books and add them to your public profile. Then browse and borrow books from other people's bookshelves!",
+                "A book-sharing platform made easy: use your webcam to scan your books and add them to your public profile. Then browse and borrow books from other people's bookshelves! A collaboration with Michel W., aka Tiefengnom.",
             github: "https://github.com/Tiefengnom/BBFront",
             demo: "https://bbookbandits.netlify.app/",
         },
         {
             id: 2,
             image: BB,
-            title: "Bookbandits",
+            title: "My Daily News",
             description:
-                "A book-sharing platform made easy: use your webcam to scan your books and add them to your public profile. Then browse and borrow books from other people's bookshelves!",
-            github: "https://github.com/Tiefengnom/BBFront",
-            demo: "https://bbookbandits.netlify.app/",
+                "A simple news app which draws the most important news from an API according to the categories I have set as my interests.",
+            github: "https://github.com/roxibi/my-daily-news.github.io",
+            demo: "coming soon",
         },
-        {
-            id: 3,
-            image: BB,
-            title: "Bookbandits",
-            description:
-                "A book-sharing platform made easy: use your webcam to scan your books and add them to your public profile. Then browse and borrow books from other people's bookshelves!",
-            github: "https://github.com/Tiefengnom/BBFront",
-            demo: "https://bbookbandits.netlify.app/",
-        },
-        {
-            id: 4,
-            image: BB,
-            title: "Bookbandits",
-            description:
-                "A book-sharing platform made easy: use your webcam to scan your books and add them to your public profile. Then browse and borrow books from other people's bookshelves!",
-            github: "https://github.com/Tiefengnom/BBFront",
-            demo: "https://bbookbandits.netlify.app/",
-        },
+        // {
+        //     id: 3,
+        //     image: BB,
+        //     title: "Bookbandits",
+        //     description:
+        //         "A book-sharing platform made easy: use your webcam to scan your books and add them to your public profile. Then browse and borrow books from other people's bookshelves!",
+        //     github: "https://github.com/Tiefengnom/BBFront",
+        //     demo: "https://bbookbandits.netlify.app/",
+        // },
+        // {
+        //     id: 4,
+        //     image: BB,
+        //     title: "Bookbandits",
+        //     description:
+        //         "A book-sharing platform made easy: use your webcam to scan your books and add them to your public profile. Then browse and borrow books from other people's bookshelves!",
+        //     github: "https://github.com/Tiefengnom/BBFront",
+        //     demo: "https://bbookbandits.netlify.app/",
+        // },
     ];
 
     return (
