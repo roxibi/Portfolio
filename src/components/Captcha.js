696fc92d4621ab19d9d61930dd6a5dcb2fea2d59
Roxana Bita <roxana.bita@gmail.com>
2023-02-22 22:44:16 +0100

    add captcha with math

---
+++ src/components/Captcha.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+const Captcha = () => {
+
+const num1=Math.floor(Math.random() * 25);
+const num2=Math.floor(Math.random() * 25);
+const sum=num1+num2
+const operation=num1+"+"+num2;
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Captcha
+
+
+
+
+
+
+
+
+
+
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,20 +5,33 @@ import "./contact.css";
 const Contact = () => {
     const form = useRef();
     const [submitted, setSubmitted] = useState(false);
+    const [answer, setAnswer] = useState();
+    const [num1, setNum1] = useState(Math.floor(Math.random() * 25));
+    const [num2, setNum2] = useState(Math.floor(Math.random() * 25));
+
+    const refresh = (e) => {
+        e.preventDefault();
+        setNum1(Math.floor(Math.random() * 25));
+        setNum2(Math.floor(Math.random() * 25));
+    };
 
     const sendEmail = (e) => {
         e.preventDefault();
-
-        emailjs.sendForm("service_arvfwvn", "template_k7qfd84x", form.current, "TkdceoCwkSVemG6s6").then(
-            (result) => {
-                console.log(result.text);
-                e.target.reset();
-                setSubmitted(true);
-            },
-            (error) => {
-                console.log(error.text);
-            }
-        );
+        console.log(answer, num1, num2);
+        if (parseInt(answer) === num1 + num2) {
+            emailjs.sendForm("service_arvfwvn", "template_k7qfd84x", form.current, "TkdceoCwkSVemG6s6").then(
+                (result) => {
+                    console.log(result.text);
+                    e.target.reset();
+                    setSubmitted(true);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+        } else {
+            alert("something went wrong yo");
+        }
     };
 
     return (
@@ -62,9 +75,14 @@ const Contact = () => {
                     <label>Email</label>
                     <input type='email' name='user_email' placeholder='Your Email' required />
                     <label>Message</label>
-
                     <textarea name='message' rows='7' placeholder='Your Message'></textarea>
-
+                 <div className="captcha">
+                    <p >
+                        {" "}
+                        {num1}+{num2}
+                    </p>{" "}
+                    <button className="btn refresh" onClick={refresh}><i class="fa fa-refresh" aria-hidden="true"></i></button> </div>
+                    <input type='number' onChange={(e) => setAnswer(e.target.value)}></input>
                     <input type='submit' className='btn btn-primary' value='Send'></input>
                     {submitted && <p>Thank you for contacting me! I will get back to you as soon as possible.</p>}
                 </form>
