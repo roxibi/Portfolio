c16d7cafc44efbeb94070db9d973a807b56d39d4
Roxana Bita <roxana.bita@gmail.com>
2023-02-24 14:24:18 +0100

    add Zappys, delete dupicate css

--- src/components/porfolio/Portfolio.jsx
+++ src/components/porfolio/Portfolio.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./portfolio.css";
 import BB from "../../assets/mockup-bb.png";
 import News from "../../assets/mockup-news.png";
+import Zappys from "../../assets/mockup-math.png"
 
 const Portfolio = () => {
     const projects = [
         {
             id: 1,
             image: BB,
-            stack:["React" , "Tailwind" , "NodeJS" , "ExpressJS" , "MongoDB","xzing library" , "Google Books API" , "Netlify" , "Cyclic"],
+            stack:["React" , "JavaScript", "Tailwind" , "NodeJS" , "ExpressJS" , "MongoDB","xzing library" , "Google Books API" , "Netlify" , "Cyclic"],
             title: "Bookbandits",
             description:
                 "A book-sharing platform made easy: use your webcam to scan your books and add them to your public profile. Then browse and borrow books from other people's bookshelves! A collaboration with Michel W., aka Tiefengnom.",
@@ -19,21 +20,22 @@ const Portfolio = () => {
             id: 2,
             image: News,
             stack:["React" , "CSS" , "NodeJS" , "ExpressJS" , "NewsAPI" , "Netlify" , "Cyclic"],
-            title: "My Daily News",
+            title: "Daily News",
             description:
                 "A simple news app which draws the most important news from an API according to the categories I have set as my interests. Live Demo coming soon! See my backend code, which is running on Cyclic, here: https://github.com/roxibi/backend-daily-news.",
             github: "https://github.com/roxibi/my-daily-news.github.io",
             demo: "https://my-daily-news.netlify.app/",
         },
-        // {
-        //     id: 3,
-        //     image: BB,
-        //     title: "Bookbandits",
-        //     description:
-        //         "A book-sharing platform made easy: use your webcam to scan your books and add them to your public profile. Then browse and borrow books from other people's bookshelves!",
-        //     github: "https://github.com/Tiefengnom/BBFront",
-        //     demo: "https://bbookbandits.netlify.app/",
-        // },
+        {
+            id: 3,
+            image: Zappys,
+            stack:["React" , "JavaScript", "CSS" , "emailjs", "jspdf" , "Netlify"],
+            title: "Zappys Math Generator",
+            description:
+                "Made for a teacher at a Toronto highschool, following her specifications and needs I made an app which generates randomised math operations, giving the option to set the operation sign and to download a layouted PDF of the worksheet.",
+            github: "https://github.com/roxibi/Zappys-Math-Generator",
+            demo: "https://zappys.netlify.app/",
+        },
         // {
         //     id: 4,
         //     image: BB,
